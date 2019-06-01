dec30e59004f31c71e5ba213cca5d09a31d19486
unknown <ccshnue@gmail.com>
2019-06-01 10:21:03 +0700

    add test fix

--- phan2/Test.java
+++ phan2/Test.java
@@ -2,12 +2,15 @@ package phan2;
 
 public class Test {
 
+	public Shape s1;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Shape s1 = new Circle(3.5);
-		System.out.println(s1.calcArea());
-		Shape s2 = new Rectangle(3,4);
-		System.out.println(s2.calcArea());
+		Test test = new Test();
+		test.s1 = new Circle(3.5);
+		System.out.println(test.s1.calcArea());
+//		test.s1 = new Rectangle(3,4);
+//		System.out.println(test.s1.calcArea());
 	}
 
 }
